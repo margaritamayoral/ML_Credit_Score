ab0e6363cf0f4ad5a3750e86b7f511ce74438eb2
margaritamayoralvilla <margaritamayoralvilla@gmail.com>
2019-03-01 18:02:26 -0500

    second commit, more descriptive statistics and data description

--- python_files/RF_1.py
+++ python_files/RF_1.py
@@ -38,7 +38,7 @@ ax3 = sns.countplot(x="Risk", hue="Age", data=df)
 plt.savefig('../output/cnt_risk_by_age.png')
 ax4 = plt.figure(4)
 ax4 = sns.catplot(x="Age", hue="Sex", col="Risk", data=df, kind="count", height=4, aspect=.7);
-plt.savefig('../output/cnt_risk_sex_age.png');
+plt.savefig('../output/cnt_risk_sex_age.png')
 #####
 df_good = df.loc[df["Risk"] == 'good']['Age'].values.tolist()
 df_bad = df.loc[df["Risk"] == 'bad']['Age'].values.tolist()
@@ -113,11 +113,154 @@ ax15 = sns.violinplot(x="Job", y="Credit amount", hue="Risk", data=df)
 plt.savefig("../output/job_vs_risk_violinplot.png")
 
 ax16 = plt.figure(16)
-fig, ax16 = plt.subplots(figsize(12, 12), nrows=2)
-ax16 = sns.boxplot(x="Job", y="Credit amount", hue="Risk", palette="hls", ax16=ax16[0], data=df)
+fig, ax = plt.subplots(figsize=(12, 12), nrows=2)
+ax16 = sns.boxplot(x="Job", y="Credit amount", hue="Risk", palette="hls", ax=ax[0], data=df)
 ax16.set_title("Credit Amount by Job", fontsize=15)
 ax16.set_xlabel("Job Reference", fontsize=12)
 ax16.set_ylabel("Credit Amount", fontsize=12)
+
+ax17 = sns.violinplot(x="Job", y="Age", hue="Risk", split=True, pallete="hls", ax=ax[1], data=df)
+ax17.set_title("Job class vs Age", fontsize=15)
+ax17.set_xlabel("Job class", fontsize=12)
+ax17.set_ylabel("Age", fontsize=12)
+plt.subplots_adjust(hspace=0.4, top=0.9)
+plt.savefig("../output/job_vs_age_credit_amount_risk.png")
+#plt.show()
+
+#Visualizing the distribution of credit amount
+
+#Histogram:
+x1 = np.log(df.loc[df["Risk"] == 'good']['Credit amount'])
+x2 = np.log(df.loc[df["Risk"] == 'bad']['Credit amount'])
+#histo = [x1, x2]
+#group_labels = ["Good credit", "Bad credit"]
+
+ax18 = plt.figure(18)
+#ax18 = sns.distplot(x1)
+ax18 = sns.distplot(x1, label="Good credit",  color="r")
+ax18 = sns.distplot(x2, label="Bad credit", color="b")
+ax18.set_title("Credit amount distribution", fontsize=15)
+ax18.set_xlabel("Good Credit (red), Bad Credit (blue)", fontsize=12)
+ax18.set_ylabel("Credit amount", fontsize=12)
+plt.savefig("../output/Credit_amount_distribution.png")
+
+### Distribution of saving accounts by risk
+print("Description of distribution of Saving Accounts by Risk")
+print(pd.crosstab(df["Saving accounts"], df.Risk))
+
+ax19 = plt.figure(19)
+fig, ax = plt.subplots(3, 1, figsize=(12, 12))
+ax19 = sns.countplot(x="Saving accounts", hue="Risk", palette="hls", ax=ax[0], data=df)
+ax19.set_title("Count saving accounts", fontsize=15)
+ax19.set_xlabel("Saving account class", fontsize=12)
+ax19.set_ylabel("Count", fontsize=12)
+
+ax20 = sns.boxplot(x="Saving accounts", y="Credit amount", hue="Risk", palette="hls", ax=ax[1], data=df)
+ax20.set_title("Credit amount by Savings accounts", fontsize=15)
+ax20.set_xlabel("Saving account class", fontsize=12)
+ax20.set_ylabel("Credit amount", fontsize=12)
+
+ax21 = sns.boxplot(x="Saving accounts", y="Age", hue="Risk", palette="hls", data=df)
+ax21.set_title("Savings accounts and Age relation", fontsize=15)
+ax21.set_xlabel("Saving account class", fontsize=12)
+ax21.set_ylabel("Age", fontsize=12)
+
+plt.subplots_adjust(hspace=0.4, top=0.9)
+plt.savefig("../output/Saving_account_relations_distribution.png")
+
+#### Distribution of purposes
+
+print("Description of distribution of purposes")
+print(pd.crosstab(df["Purpose"], df.Risk))
+
+ax22 = plt.figure(22)
+fig, ax = plt.subplots(3, 1, figsize=(12, 12))
+ax22 = sns.countplot(x="Purpose", hue="Risk", palette="hls", ax=ax[0], data=df)
+ax22.set_title("Count Purposes", fontsize=15)
+ax22.set_xlabel("Purpose class", fontsize=12)
+ax22.set_ylabel("Count", fontsize=12)
+
+ax23 = sns.boxplot(x="Purpose", y="Credit amount", hue="Risk", palette="hls", ax=ax[1], data=df)
+ax23.set_title("Credit amount by Purpose", fontsize=15)
+ax23.set_xlabel("Purpose class", fontsize=12)
+ax23.set_ylabel("Credit amount", fontsize=12)
+
+ax24 = sns.boxplot(x="Purpose", y="Age", hue="Risk", palette="hls", data=df)
+ax24.set_title("Purpose and Age relation", fontsize=15)
+ax24.set_xlabel("Purpose class", fontsize=12)
+ax24.set_ylabel("Age", fontsize=12)
+
+plt.subplots_adjust(hspace=0.4, top=0.9)
+plt.savefig("../output/purpose_vs_age_credit_amount_risk.png")
+
+## Duration of the loans distribution and density
+df_good = df.loc[df["Risk"] == 'good']
+df_bad = df.loc[df["Risk"] == 'bad']
+
+ax25 = plt.figure(25)
+fig, ax = plt.subplots(3, 1, figsize=(12, 14))
+ax25 = sns.countplot(x="Duration", hue="Risk", palette="hls", ax=ax[0], data=df)
+ax25.set_title("Duration Count", fontsize=20)
+ax25.set_xlabel("Duration Distribution", fontsize=12)
+ax25.set_ylabel("Count", fontsize=12)
+
+ax26 = sns.pointplot(x="Duration", y="Credit amount", hue="Risk", palette="hls", ax=ax[1], data=df)
+ax26.set_title('Credit amount distribution by duration', fontsize=20)
+ax26.set_xlabel("Duration", fontsize=12)
+ax26.set_ylabel("Credit amount", fontsize=12)
+
+
+ax27 = sns.distplot(df_good["Duration"], color="g")
+ax27 = sns.distplot(df_bad["Duration"], color="r")
+ax27.set_title("Duration of credit vs good or bad behaviour", fontsize=15)
+ax27.set_xlabel("Duration", fontsize=12)
+ax27.set_ylabel("Frequency", fontsize=12)
+plt.subplots_adjust(wspace=0.4, hspace=0.4, top=0.9)
+plt.savefig("../output/Credit_Duration_vs_Credit_Behaviour.png")
+
+## Checking Account variable
+
+### Checking the distribution
+
+ax28 = plt.figure(28)
+fig, ax = plt.subplots(3, 1, figsize=(12, 14))
+ax28 = sns.countplot(x="Checking account", hue="Risk", palette="hls", ax=ax[0], data=df)
+ax28.set_title("Checking account vs Risk", fontsize=15)
+ax28.set_xlabel("Checking account class", fontsize=12)
+ax28.set_ylabel("Count", fontsize=12)
+
+ax29 = sns.boxplot(x="Checking account", y = "Credit amount", hue="Risk", palette="hls", data=df, ax=ax[1])
+ax29.set_title("Checking account distribution in function of Credit Amount", fontsize=15)
+ax29.set_xlabel("Checking account", fontsize=12)
+ax29.set_ylabel("Credit amount", fontsize=12)
+
+ax30 = sns.violinplot(x="Checking account", y="Age", hue="Risk", palette="hls", data=df, ax=ax[2])
+ax30.set_title("Checking account vs Age", fontsize=15)
+ax30.set_xlabel("Checking account class", fontsize=12)
+ax30.set_ylabel("Age", fontsize=12)
+
+plt.subplots_adjust(wspace=0.4, hspace=0.4, top=0.9)
+plt.savefig("../output/Checking_account_vs_Risk.png")
+
+### Crosstab to explain more insights in the data
+
+print(pd.crosstab(df.Sex, df.Job))
+
+ax31 = plt.figure(31)
+ax31 = sns.violinplot(x="Housing", y="Age", hue="Risk", palette="hls", data=df)
+ax31.set_title("Exploring Housing vs Age", fontsize=15)
+ax31.set_xlabel("Housing", fontsize=12)
+ax31.set_ylabel("Age", fontsize=12)
+plt.savefig("../output/Housing_Age.png")
+
+print(pd.crosstab(df.["Checking account"], df.Sex))
+
+date_int = ["Purpose", "Sex"]
+cm = sns.light_palette("green", as_cmap=True)
+pd.crosstab(df[date_int[0]], df)
+
+
+
 
 
 
